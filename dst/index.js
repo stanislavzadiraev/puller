d2084214dcaadb576ee62d5070b1c8e2432e1ee9
stanislavzadiraev <zadiraeff@yandex.ru>
2023-11-04 00:13:08 +0300

    поехали

---
+++ dst/index.js
@@ -0,0 +1,6 @@
+const { exec } = require('node:child_process');
+
+const index = () =>
+    readFile('package.json')
+        .then(data => JSON.parse(data))
+        .then(json => console.log(json))
